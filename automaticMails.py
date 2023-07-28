786a6b7a9e0dd9c5f48f831e61ae80768452aa7e
AkshayChemuri <chemuriakshay@gmail.com>
2023-07-28 15:49:24 -0500

    added content

--- automaticMails.py
+++ automaticMails.py
@@ -25,7 +25,7 @@ pswd = "your password"
 #here password is not your email password it is generated using google setting
 #below steps to enable app password in gmail account
 #go to the google account setting next go to security tab
-#after that enable 2-step Verification  and make sure to on it.
+#after that enable 2-step Verification  and make sure to on it. 
 # after in the search bar please enter app password
 #create a custom name to and click on generate then you can see password as abcd abcd abcd abcd
 #enter this password in the pswd variable.
